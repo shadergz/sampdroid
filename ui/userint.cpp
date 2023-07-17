f950429ce53de6620f8117d6e65e60d647460189
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-07-17 19:05:01 -0300

    Adds `Dear imgui` layer into plugin UI class, problems ahead...

--- ui/userint.cpp
+++ ui/userint.cpp
@@ -1,10 +1,24 @@
+
 #include <ui/userint.h>
+#include <imgui/imgui.h>
+
+#include <log_client.h>
 
 UiClientUser::UiClientUser()
 {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io{ImGui::GetIO()};
+
+    // Dark mode isn't enable by default
+    ImGui::StyleColorsDark();
+    salog::printFormat(ANDROID_LOG_INFO, "GUI: ImGUI version in use %d: %s", 
+        IMGUI_VERSION_NUM, IMGUI_VERSION);
 
 }
 UiClientUser::~UiClientUser()
 {
+    salog::print(ANDROID_LOG_INFO, "GUI system is been shutdown now!");
+    ImGui::DestroyContext();
 
 }
